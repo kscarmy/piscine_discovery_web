e146c180c538d2a534ec91467179a52af8ee708a
kscarmy <guderram@e2r2p15.clusters.42paris.fr>
2022-10-27 11:27:34 +0200

    Push 10 : cell03 : ex00 finit.

---
+++ cell03/ex00/background.js
@@ -0,0 +1,9 @@
+function background()	{
+	document.body.style.backgroundColor = setBg();
+}
+
+const setBg = () => {
+	const randomColor = Math.floor(Math.random()*16777215).toString(16);
+	document.body.style.backgroundColor = "#" + randomColor;
+	color.innerHTML = "#" + randomColor;
+}
